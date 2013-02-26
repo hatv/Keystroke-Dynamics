de0c4533fccd20e3f5a98b402e5a80051b1c7d18
Victor Hatinguais <contact@victorhatinguais.fr>
2013-02-26 14:45:12 -0500

    Improvement and documentation of the Applet

--- KeyboardApplet/src/KeyboardApplet.java
+++ KeyboardApplet/src/KeyboardApplet.java
@@ -5,9 +5,12 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.LinkedList;
 import java.util.List;
-
 import netscape.javascript.JSObject;
 
+/**
+ * Java Applet which access the keystrokes times directly from the OS data.
+ * @author Victor Hatinguais, www.victorhatinguais.fr
+ */
 @SuppressWarnings("serial")
 public class KeyboardApplet extends Applet implements KeyListener {
 
@@ -20,11 +23,17 @@ public class KeyboardApplet extends Applet implements KeyListener {
 	//List<Long> typed = new LinkedList<Long>();
 	boolean newData = false;
 
+	/**
+	 * Initialize the Applet.
+	 */
 	public void init() {
 		setBackground(Color.lightGray);
 		addKeyListener(this);
 	}
 
+	/**
+	 * Action which is done on key press event.
+	 */
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyChar() != KeyEvent.VK_ENTER) {
 			long tsp = e.getWhen();
@@ -33,10 +42,14 @@ public class KeyboardApplet extends Applet implements KeyListener {
 			pressed.add(tsp - first);
 		}
 	}
+
+	/**
+	 * Action which is done on key release event.
+	 */
 	public void keyReleased(KeyEvent e) {
 		char c = e.getKeyChar();
 		long tsp = e.getWhen();
-		if (c == KeyEvent.VK_ENTER) {
+		if (c == KeyEvent.VK_ENTER || c == KeyEvent.VK_BACK_SPACE) {
 			i++;
 			System.out.println("chars    " + characters);
 			//System.out.println("typed    " + typed);
@@ -49,28 +62,33 @@ public class KeyboardApplet extends Applet implements KeyListener {
 			released.add(tsp - first);
 		}
 	}
+
+	/**
+	 * Action which is done on key typed event.
+	 */
 	public void keyTyped(KeyEvent e) {
 		char c = e.getKeyChar();
+		int code = e.getKeyCode();
 		//long tsp = e.getWhen();
-		if (c != KeyEvent.CHAR_UNDEFINED && c != KeyEvent.VK_ENTER) {
+		System.out.println(code);
+		// keyPress and release only
+		if ((code >= KeyEvent.VK_A && code <= KeyEvent.VK_Z)
+				|| code == KeyEvent.VK_SPACE) {
 			//typed.add(tsp - first);
 			characters.add(c);
 			string = string + c;
+			this.callJSCharacter(Character.toString(c));
 		}
 	}
-	public void callJS() {
+
+	/**
+	 * Call the JavaScript function <appletCallback> which has to be defined
+	 * on the web page which display the Applet.
+	 * Send the entire word or passphrase or typing sequence to the JS.
+	 */
+	public void callJSString() {
 		try {
 			JSObject window = JSObject.getWindow(this);
-			/*
-			window.call("appletCallback",
-					new String[] {"chars    " + characters.toString()});
-			window.call("appletCallback",
-					new String[] {"typed    " + typed.toString()});
-			window.call("appletCallback",
-					new String[] {"pressed  " + pressed.toString()});
-			window.call("appletCallback",
-					new String[] {"released " + released.toString()});
-			*/
 			window.call("appletCallback",
 					new String[] {string + ";" + pressed.toString() + ";"
 							+ released.toString()});
@@ -78,6 +96,25 @@ public class KeyboardApplet extends Applet implements KeyListener {
 			//e.printStackTrace();
 		}
 	}
+
+	/**
+	 * Call the JavaScript function <appletCallbackChar> which has to be defined
+	 * on the web page which display the Applet.
+	 * Send the last typed character to the JS.
+	 */
+	public void callJSCharacter(String c) {
+		try {
+			JSObject window = JSObject.getWindow(this);
+			window.call("appletCallbackChar",
+					new String[] {c});
+		} catch (Exception e) {
+			//e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Define what is displayed on the Applet.
+	 */
 	public void paint(Graphics g) {
 		if (newData) {
 			g.setColor(Color.black);
@@ -85,7 +122,7 @@ public class KeyboardApplet extends Applet implements KeyListener {
 			//g.drawString("typed    " + typed.toString(), 5, 30);
 			g.drawString("pressed  " + pressed.toString(), 5, 30);
 			g.drawString("released " + released.toString(), 5, 45);
-			this.callJS();
+			this.callJSString();
 			first = 0;
 			characters.clear();
 			string = "";
