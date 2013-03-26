a80e3e102f233cf0c738ed2cd7c91859aafefec0
Victor Hatinguais <contact@victorhatinguais.fr>
2013-03-26 18:41:28 -0400

    ServiceFailurePopup : fade in and out effects are good

--- Keystroke Dynamics/src/fr/vhat/keydyn/client/ServiceFailurePopup.java
+++ Keystroke Dynamics/src/fr/vhat/keydyn/client/ServiceFailurePopup.java
@@ -3,26 +3,41 @@ package fr.vhat.keydyn.client;
 import java.math.BigDecimal;
 
 import com.google.gwt.user.client.Timer;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.PopupPanel;
 
-public class ServiceFailurePopup {
+public class ServiceFailurePopup extends PopupPanel {
 
-	private PopupPanel popup;
-
-    protected final int SHOW_TIMER_DELAY = 40; //ms 
+	// Show timer for fade in effect
+    protected final int SHOW_TIMER_DELAY = 5; // milliseconds
     private Timer showTimer;
 
-    protected final int HIDE_TIMER_DELAY = 40; //ms
+    // Hide timer for fade out effect
+    protected final int HIDE_TIMER_DELAY = 10; // milliseconds
     private Timer hideTimer;
 
-    private static final BigDecimal DELTA = BigDecimal.valueOf(1, 1);
+    // Opacity steps
+    private static final BigDecimal DELTA = BigDecimal.valueOf(2, 2);
 
+    /**
+     * Constructor.
+     * @param message HTML message to display in the popup.
+     * @param timeout Delay before the popup automatically hide itself.
+     */
     public ServiceFailurePopup(String message) {
-        this.popup = new PopupPanel(true, false);
-        this.popup.setWidget(new Label(message));
-        //this.popup.setGlassEnabled(true);
-        this.popup.center();
+    	this(message, 5000);
+    }
+
+    /**
+     * Constructor.
+     * @param message HTML message to display in the popup.
+     * @param timeout Delay before the popup automatically hide itself.
+     */
+    public ServiceFailurePopup(String message, int timeout) {
+        super(true, false);
+        this.setWidget(new HTML(message));
+        this.setGlassEnabled(true);
+        this.center();
 
         Timer timer = new Timer()
         {
@@ -32,66 +47,76 @@ public class ServiceFailurePopup {
                 hidePopup();
             }
         };
-        timer.schedule(3000);
+        timer.schedule(timeout);
 
         this.showTimer = new Timer() {
             @Override
             public void run() {
-                String opacityStr = popup.getElement().getStyle().getOpacity();
-                boolean increased = false;
-                if (!isBlank(opacityStr)) {
-                    try {
-                        BigDecimal opacity = new BigDecimal(opacityStr);
-                        if (opacity.compareTo(new BigDecimal(1)) < 0) {
-                             popup.getElement().getStyle().setOpacity(opacity.add(DELTA).doubleValue());
-                             increased = true;
-                         }
-                     } catch (NumberFormatException nfe) {
- // fallback to showing
-                     }
-                 }
-                 if (!increased) {
-                     popup.getElement().getStyle().setOpacity(1);
-                     cancel();
-                 }
-             }
-         };
+            	String opacityStr = getElement().getStyle().getOpacity();
+            	boolean increased = false;
+            	if (!isBlank(opacityStr)) {
+            		try {
+            			BigDecimal opacity =
+            					new BigDecimal(opacityStr);
+            			if (opacity.compareTo(new BigDecimal(1)) < 0) {
+            				getElement().getStyle().setOpacity(
+            						opacity.add(DELTA).doubleValue());
+            				getGlassElement().getStyle().setOpacity(
+            						opacity.doubleValue()/4);
+            				increased = true;
+            			}
+            		} catch (NumberFormatException nfe) {
+            			// Fallback to showing
+            		}
+            	}
+            	if (!increased) {
+            		getElement().getStyle().setOpacity(1);
+            		getGlassElement().getStyle().setOpacity(1./4);
+            		cancel();
+            	}
+            }
+        };
+
          this.hideTimer = new Timer() {
              @Override
              public void run() {
-                 String opacityStr = popup.getElement().getStyle().getOpacity();
+                 String opacityStr = getElement().getStyle().getOpacity();
                  boolean decreased = false;
                  if (!isBlank(opacityStr)) {
                      try {
-                         BigDecimal opacity = new BigDecimal(opacityStr);
-                         if (opacity.compareTo(DELTA) > 0) {
-                            popup.getElement().getStyle().setOpacity(opacity.subtract(DELTA).doubleValue());
-                            decreased = true;
-                        }
-                    } catch (NumberFormatException nfe) {
- // fallback to hiding
-                    }
-                }
-                if (!decreased) {
-                    popup.hide();
-                    cancel();
-                }
-            }
-        };
+                    	 BigDecimal opacity = new BigDecimal(opacityStr);
+                    	 if (opacity.compareTo(DELTA) > 0) {
+                    		 getElement().getStyle().setOpacity(
+                    				 opacity.subtract(DELTA).doubleValue());
+                    		 getGlassElement().getStyle().setOpacity(
+                    				 opacity.doubleValue()/4);
+                    		 decreased = true;
+                         }
+                     } catch (NumberFormatException nfe) {
+                    	 // Fallback to hiding
+                     }
+                 }
+                 if (!decreased) {
+                	 hide();
+                	 cancel();
+                 }
+             }
+         };
     }
-        public void showPopup(){
-            popup.getElement().getStyle().setOpacity(0);
-            popup.show();
-            showTimer.scheduleRepeating(SHOW_TIMER_DELAY);
-        }
 
-        public void hidePopup(){
-            showTimer.cancel();
-            hideTimer.scheduleRepeating(HIDE_TIMER_DELAY);
-        }
+    public void showPopup(){
+    	getElement().getStyle().setOpacity(0);
+    	getGlassElement().getStyle().setOpacity(0);
+    	show();
+    	showTimer.scheduleRepeating(SHOW_TIMER_DELAY);
+    }
 
+    public void hidePopup(){
+    	showTimer.cancel();
+    	hideTimer.scheduleRepeating(HIDE_TIMER_DELAY);
+    }
 
     public static boolean isBlank(final String str) {
-        return (str == null) || (str.trim().length() == 0);
+    	return (str == null) || (str.trim().length() == 0);
     }
-  }
+}
